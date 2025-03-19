6e62e95f5d2d264cdbd41fca83a360edb0b3318a
Hazelya <hazelya.hp@gmail.com>
2025-03-19 13:29:25 +0100

    changement setup

--- setup.py
+++ setup.py
@@ -4,22 +4,16 @@ setup(
     name="packageGES",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[
+    install_requires=[ # les packages nécéssaire au code
         "pandas",
         "openpyxl"
     ],
     include_package_data=True, # Permet d'inclure des fichiers non-Python
-    author="Ton Nom",
-    author_email="ton.email@example.com",
+    author="Julia",
     description="Un package Python pour calculer les GES",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/ton-utilisateur/packageGES",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+    url="https://github.com/Hazelya/packageGES",
     python_requires=">=3.7",
 )
 
